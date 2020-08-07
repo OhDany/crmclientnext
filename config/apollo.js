d3c66678fd33fb4a63e9cadb4ed1f76b4c4bcfc9
Daniel Ortega <oh.dany.mx@gmail.com>
2020-08-07 09:39:28 -0500

    Configurar Apollo Client

---
+++ config/apollo.js
@@ -0,0 +1,12 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import fetch from 'node-fetch';
+
+const client = new ApolloClient({
+  connectToDevTools: true,
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://loacalhost:4000/',
+  }),
+});
+
+export default client;
