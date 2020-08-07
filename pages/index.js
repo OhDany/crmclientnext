157aef386ed643545d6cac89c4d81e2d51fa564f
Daniel Ortega <oh.dany.mx@gmail.com>
2020-08-07 17:45:56 -0500

    Obtener los Clientes del Vendedodr

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,54 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
+import { useQuery, gql } from '@apollo/client';
+
+const OBTENER_CLIENTE_USUARIO = gql`
+  query obtenerClientesVendedor {
+    obtenerClientesVendedor {
+      id
+      nombre
+      apellido
+      empresa
+      email
+    }
+  }
+`;
 
 const Index = () => {
+  // Condulata de Apollo
+  const { data, loading, error } = useQuery(OBTENER_CLIENTE_USUARIO);
+
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
+  if (loading) return 'Cargando...';
+
   return (
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
+
+        <table className="table-auto shadow-md mt-10 w-full w-lg">
+          <thead className="bg-gray-800">
+            <tr className="text-white">
+              <th className="w-1/5 py-2">Nombre</th>
+              <th className="w-1/5 py-2">Empresa</th>
+              <th className="w-1/5 py-2">Email</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {data.obtenerClientesVendedor.map((cliente) => (
+              <tr key={cliente.id}>
+                <td className="border px-4 py-2">
+                  {cliente.nombre} {cliente.apellido}
+                </td>
+                <td className="border px-4 py-2">{cliente.empresa}</td>
+                <td className="border px-4 py-2">{cliente.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </Layout>
     </div>
   );
