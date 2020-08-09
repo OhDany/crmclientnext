56f58af97469f4be80f9c25c1820c0f85c778156
Daniel Ortega <oh.dany.mx@gmail.com>
2020-08-09 11:51:05 -0500

    Creando el formulario para Nuevos Clientes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout from '../components/Layout';
 import { useQuery, gql } from '@apollo/client';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const OBTENER_CLIENTES_USUARIO = gql`
   query obtenerClientesVendedor {
@@ -34,6 +35,12 @@ const Index = () => {
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
+
+        <Link href="/nuevocliente">
+          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 uppercase font-bold w-full lg:w-auto text-center">
+            Nuevo Cliente
+          </a>
+        </Link>
 
         <table className="table-auto shadow-md mt-10 w-full w-lg">
           <thead className="bg-gray-800">
