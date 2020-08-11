a928950e2e91941c66efa8ea3f51b0685733b014
Daniel Ortega <oh.dany.mx@gmail.com>
2020-08-11 11:44:47 -0500

    Obtener Productos Inicial

--- pages/productos.js
+++ pages/productos.js
@@ -1,10 +1,48 @@
 import Layout from '../components/Layout';
+import Producto from '../components/Producto';
+import { useQuery, gql } from '@apollo/client';
+
+const OBTENER_PRODUCTOS = gql`
+  query obtenerProductos {
+    obtenerProductos {
+      id
+      nombre
+      precio
+      existencia
+      creado
+    }
+  }
+`;
 
 const Productos = () => {
+  // Cunsulatar los productos
+  const { data, loading, error } = useQuery(OBTENER_PRODUCTOS);
+
+  // console.log(data);
+  // console.log(loading);
+  // console.log(error);
+  if (loading) return 'Cargando...';
   return (
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+
+        <table className="table-auto shadow-md mt-10 w-full w-lg">
+          <thead className="bg-gray-800">
+            <tr className="text-white">
+              <th className="w-1/5 py-2">Nombre</th>
+              <th className="w-1/5 py-2">Existencia</th>
+              <th className="w-1/5 py-2">Precio</th>
+              <th className="w-1/5 py-2">Elimiar</th>
+              <th className="w-1/5 py-2">Editar</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {data.obtenerProductos.map((producto) => (
+              <Producto key={producto.id} producto={producto} />
+            ))}
+          </tbody>
+        </table>
       </Layout>
     </div>
   );
